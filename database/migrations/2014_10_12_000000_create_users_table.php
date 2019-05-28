9bb873ca6798d11510135b1b0dba62888cfb3257
ln8198 <ln714360720@163.com>
2019-05-28 17:34:04 +0800

    zhihu_app init

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('email')->unique();
+            $table->string('avatar');
+            $table->integer('questions_count')->default(0);
+            $table->integer('answers_count')->default(0);
+            $table->integer('comments_count')->default(0);
+            $table->integer('favorites_count')->default(0)->comment('收藏数');
+            $table->integer('likes_count')->default(0)->comment('点赞数');
+            $table->integer('followers_count')->default(0)->comment('关注数');
+            $table->integer('followings_count')->default(0)->comment('被关注数');
+            $table->json('setting')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
